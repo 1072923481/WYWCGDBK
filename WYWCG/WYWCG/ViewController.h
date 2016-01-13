1947592cc0b06b9b90c512ad83b8ef346a1d6941
1072923481 <1072923481@qq.com>
2016-01-13 14:25:42 +0800

    创建新工程

---
+++ WYWCG/WYWCG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYWCG
+//
+//  Created by ma c on 16/1/13.
+//  Copyright (c) 2016年 王岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WYWCG/WYWCGTests/WYWCGTests.m
@@ -0,0 +1,40 @@
+//
+//  WYWCGTests.m
+//  WYWCGTests
+//
+//  Created by ma c on 16/1/13.
+//  Copyright (c) 2016年 王岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface WYWCGTests : XCTestCase
+
+@end
+
+@implementation WYWCGTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
